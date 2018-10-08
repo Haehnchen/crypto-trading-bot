4165b87c4f932126945a8219ff60e6d28997b3f7
Daniel Espendiller <daniel@espendiller.net>
2018-10-08 21:13:46 +0200

    add simple dump / pump strategy based on volume change via OBV indicator and SMA #1

---
+++ modules/strategy/strategies/obv_pump_dump.js
@@ -0,0 +1,75 @@
+'use strict';
+
+module.exports = class {
+    getName() {
+        return 'obv_pump_dump'
+    }
+
+    buildIndicator(indicatorBuilder, options) {
+        indicatorBuilder.add('obv', 'obv', '1m')
+
+        indicatorBuilder.add('ema', 'ema', '1m', {
+            'length': 200,
+        })
+    }
+
+    period(indicatorPeriod, options) {
+        let triggerMultiplier = options['trigger_multiplier'] || 2
+        let triggerTimeWindows = options['trigger_time_windows'] || 3
+
+        return new Promise((resolve) => {
+            let obv = indicatorPeriod.getIndicator('obv')
+
+            if (obv.length <= 30) {
+                resolve()
+                return
+            }
+
+            let price = indicatorPeriod.getPrice()
+
+            let ema = indicatorPeriod.getIndicator('ema').slice(-1)[0];
+
+            let result = {
+                'debug': {
+                    'obv': obv.slice(-1)[0],
+                    'ema': ema,
+                }
+            }
+
+            if (price > ema) {
+                // long
+                result['debug']['trend'] = 'up'
+
+                var before = obv.slice(-20, triggerTimeWindows * -1)
+
+                let highest = before.sort((a, b) => b - a).slice(0, triggerTimeWindows)
+                let highestOverage = highest.reduce((a, b) => a + b, 0) / highest.length
+
+                let current = obv.slice(triggerTimeWindows * -1)
+
+                let currentAverage = current.reduce((a, b) => a + b, 0) / current.length
+
+                result['debug']['highest_overage'] = highestOverage
+                result['debug']['current_average'] = currentAverage
+
+                if (currentAverage < highestOverage) {
+                    resolve(result)
+                    return
+                }
+
+                let difference = Math.abs(currentAverage / highestOverage)
+
+                result['debug']['difference'] = difference
+
+                if(difference >= triggerMultiplier) {
+                    result['signal'] = 'long'
+                }
+            } else {
+                // short
+                result['debug']['trend'] = 'down'
+            }
+
+            resolve(result)
+        })
+    }
+}
---
+++ test/modules/strategy/strategies/obv_pump_dump.test.js
@@ -0,0 +1,38 @@
+let assert = require('assert');
+let OBVPumpDump = require('../../../../modules/strategy/strategies/obv_pump_dump');
+let IndicatorBuilder = require('../../../../modules/strategy/dict/indicator_builder')
+let IndicatorPeriod = require('../../../../modules/strategy/dict/indicator_period')
+
+describe('#strategy obv_pump_dump', () => {
+    it('obv_pump_dump strategy builder', async() => {
+        let indicatorBuilder = new IndicatorBuilder()
+        let obv = new OBVPumpDump()
+
+        obv.buildIndicator(indicatorBuilder)
+        assert.equal(2, indicatorBuilder.all().length)
+    });
+
+    it('obv_pump_dump strategy long', async() => {
+        let obv = new OBVPumpDump()
+
+        let result = await obv.period(new IndicatorPeriod(394, {
+            'ema': [380, 370],
+            'obv': [-2358, -2395, -2395, -2395, -2385, -2165, -1987, -1987, -1990, -1990, -1990, -1990, -1990, -1948, -1808, -1601, -1394, -1394, -1147, 988, 3627, 6607, 11467],
+        }), {})
+
+        assert.equal('long', result['signal'])
+        assert.equal('up', result['debug']['trend'])
+    })
+
+    it('obv_pump_dump strategy long options', async() => {
+        let obv = new OBVPumpDump()
+
+        let result = await obv.period(new IndicatorPeriod(394, {
+            'ema': [380, 370],
+            'obv': [-2358, -2395, -2395, -2395, -2385, -2165, -1987, -1987, -1990, -1990, -1990, -1990, -1990, -1948, -1808, -1601, -1394, -1394, -1147, 988, 3627, 6607, 11467],
+        }), {'trigger_multiplier': 1000})
+
+        assert.equal(undefined, result['signal'])
+        assert.equal('up', result['debug']['trend'])
+    })
+})
