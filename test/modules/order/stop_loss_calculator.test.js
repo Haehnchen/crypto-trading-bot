aaa1ffcef7d565ed1e5f6e05388ae1c322c59444
Daniel Espendiller <daniel@espendiller.net>
2018-11-24 19:30:14 +0100

    fix test for stop lose calculation on a long position

--- test/modules/order/stop_loss_calculator.test.js
+++ test/modules/order/stop_loss_calculator.test.js
@@ -26,7 +26,7 @@ describe('#stop loss order calculation', function() {
             6501.76
         ))
 
-        assert.equal(result.toFixed(1), 6306.7)
+        assert.equal(result.toFixed(1), -6306.7)
 
         let result2 = await calculator.calculateForOpenPosition('noop', new Position(
             'BTCUSD',
@@ -37,7 +37,7 @@ describe('#stop loss order calculation', function() {
             6501.76
         ), {'percent': 5})
 
-        assert.equal(result2.toFixed(1), 6176.7)
+        assert.equal(result2.toFixed(1), -6176.7)
     })
 
     it('calculate stop lose for short', async () => {
