6c5215830fef582008cfe1f4be8dfa030faccad6
Daniel Espendiller <daniel@espendiller.net>
2018-12-25 11:21:48 +0100

    improve backtest chart with percent and volume

--- web/static/backtest.js
+++ web/static/backtest.js
@@ -4,18 +4,41 @@ $(function() {
     if (chart.length > 0) {
         var candles = chart.data('candles')
 
-        var margin = {top: 20, right: 20, bottom: 30, left: 50},
-            width = chart.width() - margin.left - margin.right,
-            height = 500 - margin.top - margin.bottom;
 
-        var dateFormat = d3.timeFormat("%y-%m-%d %H:%M"),
-            valueFormat = d3.format(',.2f');
+        var dim = {
+            width: chart.width(), height: 500,
+            margin: { top: 20, right: 60, bottom: 30, left: 60 },
+            ohlc: { height: 450 },
+            indicator: { height: 0, padding: 0 }
+        };
+
+        dim.plot = {
+            width: dim.width - dim.margin.left - dim.margin.right,
+            height: dim.height - dim.margin.top - dim.margin.bottom
+        };
+
+        dim.indicator.top = dim.ohlc.height+dim.indicator.padding;
+        dim.indicator.bottom = dim.indicator.top+dim.indicator.height+dim.indicator.padding;
+
+        var indicatorTop = d3.scaleLinear()
+            .range([dim.indicator.top, dim.indicator.bottom]);
+
+        var zoom = d3.zoom()
+            .on("zoom", zoomed);
 
         var x = techan.scale.financetime()
-            .range([0, width]);
+            .range([0, dim.plot.width]);
 
         var y = d3.scaleLinear()
-            .range([height, 0]);
+            .range([dim.ohlc.height, 0]);
+
+
+        var yPercent = y.copy();   // Same as y at this stage, will get a different domain later
+
+        var yInit, yPercentInit, zoomableInit;
+
+        var yVolume = d3.scaleLinear()
+            .range([y(0), y(0.4)]);
 
         var candlestick = techan.plot.candlestick()
             .xScale(x)
@@ -36,28 +59,154 @@ $(function() {
                         return 'right';
                 }
             })
-            .on("mouseenter", enter)
-            .on("mouseout", out);
+            .on("mouseenter", function enter(d) {
+                valueText.style("display", "inline");
+                valueText.text("Trade: " + d3.timeFormat("%y-%m-%d %H:%M")(d.date) + ", " + d.type + ", " + d3.format(',.2f')(d.price))
+            })
+            .on("mouseout",         function out() {
+                valueText.style("display", "none");
+            });
 
-        var xAxis = d3.axisBottom()
-            .scale(x);
+        var volume = techan.plot.volume()
+            .accessor(candlestick.accessor())   // Set the accessor to a ohlc accessor so we get highlighted bars
+            .xScale(x)
+            .yScale(yVolume);
 
-        var yAxis = d3.axisLeft()
-            .scale(y);
+        var xAxis = d3.axisBottom(x);
+
+        var timeAnnotation = techan.plot.axisannotation()
+            .axis(xAxis)
+            .orient('bottom')
+            .format(d3.timeFormat('%Y-%m-%d %H:%M'))
+            .width(120)
+            .translate([0, dim.plot.height]);
+
+        var yAxis = d3.axisRight(y);
+
+        var ohlcAnnotation = techan.plot.axisannotation()
+            .axis(yAxis)
+            .orient('right')
+            .format(d3.format(',.2f'))
+            .translate([x(1), 0]);
+
+        var closeAnnotation = techan.plot.axisannotation()
+            .axis(yAxis)
+            .orient('right')
+            .accessor(candlestick.accessor())
+            .format(d3.format(',.2f'))
+            .translate([x(1), 0]);
+
+        var percentAxis = d3.axisLeft(yPercent)
+            .tickFormat(d3.format('+.1%'));
+
+        var percentAnnotation = techan.plot.axisannotation()
+            .axis(percentAxis)
+            .orient('left');
+
+        var volumeAxis = d3.axisRight(yVolume)
+            .ticks(3)
+            .tickFormat(d3.format(",.3s"));
+
+        var volumeAnnotation = techan.plot.axisannotation()
+            .axis(volumeAxis)
+            .orient("right")
+            .width(35);
+
+        var ohlcCrosshair = techan.plot.crosshair()
+            .xScale(timeAnnotation.axis().scale())
+            .yScale(ohlcAnnotation.axis().scale())
+            .xAnnotation(timeAnnotation)
+            .yAnnotation([ohlcAnnotation, percentAnnotation, volumeAnnotation])
+            .verticalWireRange([0, dim.plot.height]);
 
         var svg = d3.select(".chart").append("svg")
-            .attr("width", width + margin.left + margin.right)
-            .attr("height", height + margin.top + margin.bottom)
-            .append("g")
-            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+            .attr("width", dim.width)
+            .attr("height", dim.height);
 
         var valueText = svg.append('text')
             .style("text-anchor", "end")
             .attr("class", "coords")
-            .attr("x", width - 5)
+            .attr("x", dim.width - 25)
             .attr("y", 15);
 
-        var accessor = candlestick.accessor();
+
+        var defs = svg.append("defs");
+
+        defs.append("clipPath")
+            .attr("id", "ohlcClip")
+            .append("rect")
+            .attr("x", 0)
+            .attr("y", 0)
+            .attr("width", dim.plot.width)
+            .attr("height", dim.ohlc.height);
+
+        defs.selectAll("indicatorClip").data([0, 1])
+            .enter()
+            .append("clipPath")
+            .attr("id", function(d, i) { return "indicatorClip-" + i; })
+            .append("rect")
+            .attr("x", 0)
+            .attr("y", function(d, i) { return indicatorTop(i); })
+            .attr("width", dim.plot.width)
+            .attr("height", dim.indicator.height);
+
+        svg = svg.append("g")
+            .attr("transform", "translate(" + dim.margin.left + "," + dim.margin.top + ")");
+
+        let title = chart.data('title');
+        if (title) {
+            svg.append('text')
+                .attr("class", "symbol")
+                .attr("x", 20)
+                .text(title);
+        }
+
+        svg.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + dim.plot.height + ")");
+
+        var ohlcSelection = svg.append("g")
+            .attr("class", "ohlc")
+            .attr("transform", "translate(0,0)");
+
+        ohlcSelection.append("g")
+            .attr("class", "axis")
+            .attr("transform", "translate(" + x(1) + ",0)")
+            .append("text")
+            .attr("transform", "rotate(-90)")
+            .attr("y", -12)
+            .attr("dy", ".71em")
+            .style("text-anchor", "end")
+            .text("Price ($)");
+
+        ohlcSelection.append("g")
+            .attr("class", "close annotation up");
+
+        ohlcSelection.append("g")
+            .attr("class", "volume")
+            .attr("clip-path", "url(#ohlcClip)");
+
+        ohlcSelection.append("g")
+            .attr("class", "candlestick")
+            .attr("clip-path", "url(#ohlcClip)");
+
+        ohlcSelection.append("g")
+            .attr("class", "percent axis");
+
+        ohlcSelection.append("g")
+            .attr("class", "volume axis");
+
+        // Add trendlines and other interactions last to be above zoom pane
+        svg.append('g')
+            .attr("class", "crosshair ohlc");
+
+        svg.append("g")
+            .attr("class", "tradearrow")
+            .attr("clip-path", "url(#ohlcClip)");
+
+        var accessor = candlestick.accessor(),
+            indicatorPreRoll = 33;  // Don't show where indicators don't have data
+
 
         var trades = []
 
@@ -82,50 +231,51 @@ $(function() {
             };
         }).sort(function(a, b) { return d3.ascending(accessor.d(a), accessor.d(b)); });
 
-        svg.append("g")
-            .attr("class", "candlestick");
+        x.domain(techan.scale.plot.time(data).domain());
+        y.domain(techan.scale.plot.ohlc(data.slice(indicatorPreRoll)).domain());
+        yPercent.domain(techan.scale.plot.percent(y, accessor(data[indicatorPreRoll])).domain());
+        yVolume.domain(techan.scale.plot.volume(data).domain());
 
-        svg.append("g")
-            .attr("class", "tradearrow");
+        svg.select("g.candlestick").datum(data).call(candlestick);
+        svg.select("g.close.annotation").datum([data[data.length-1]]).call(closeAnnotation);
+        svg.select("g.volume").datum(data).call(volume);
 
-        svg.append("g")
-            .attr("class", "x axis")
-            .attr("transform", "translate(0," + height + ")");
+        svg.select("g.crosshair.ohlc").call(ohlcCrosshair).call(zoom);
 
-        svg.append("g")
-            .attr("class", "y axis")
-            .append("text")
-            .attr("transform", "rotate(-90)")
-            .attr("y", 6)
-            .attr("dy", ".71em")
-            .style("text-anchor", "end")
-            .text("Price ($)");
+        svg.select("g.tradearrow").datum(trades).call(tradearrow);
 
-        // Data to display initially
-        draw(data.slice(), trades);
+        // Stash for zooming
+        zoomableInit = x.zoomable().domain([indicatorPreRoll, data.length]).copy(); // Zoom in a little to hide indicator preroll
+        yInit = y.copy();
+        yPercentInit = yPercent.copy();
 
-        function enter(d) {
-            valueText.style("display", "inline");
-            refreshText(d);
+        draw();
+
+        function reset() {
+            zoom.scale(1);
+            zoom.translate([0,0]);
+            draw();
         }
 
-        function out() {
-            valueText.style("display", "none");
+        function zoomed() {
+            x.zoomable().domain(d3.event.transform.rescaleX(zoomableInit).domain());
+            y.domain(d3.event.transform.rescaleY(yInit).domain());
+            yPercent.domain(d3.event.transform.rescaleY(yPercentInit).domain());
+
+            draw();
         }
 
-        function draw(data, trades) {
-            x.domain(data.map(candlestick.accessor().d));
-            y.domain(techan.scale.plot.ohlc(data, candlestick.accessor()).domain());
+        function draw() {
+            svg.select("g.x.axis").call(xAxis);
+            svg.select("g.ohlc .axis").call(yAxis);
+            svg.select("g.volume.axis").call(volumeAxis);
+            svg.select("g.percent.axis").call(percentAxis);
 
-            svg.selectAll("g.candlestick").datum(data).call(candlestick);
-            svg.selectAll("g.tradearrow").datum(trades).call(tradearrow);
-
-            svg.selectAll("g.x.axis").call(xAxis);
-            svg.selectAll("g.y.axis").call(yAxis);
-        }
-
-        function refreshText(d) {
-            valueText.text("Trade: " + dateFormat(d.date) + ", " + d.type + ", " + valueFormat(d.price));
+            // We know the data does not change, a simple refresh that does not perform data joins will suffice.
+            svg.select("g.candlestick").call(candlestick.refresh);
+            svg.select("g.close.annotation").call(closeAnnotation.refresh);
+            svg.select("g.volume").call(volume.refresh);
+            svg.select("g.tradearrow").call(tradearrow.refresh);
         }
     }
 });
