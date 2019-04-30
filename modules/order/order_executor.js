5084036133e5902b05fc1f0a6cb76233e1ea5e81
Phuc Nguyen Minh <minhphuc429@gmail.com>
2019-04-30 13:47:45 +0700

    cleanup code modules/order/order_executor.js

--- modules/order/order_executor.js
+++ modules/order/order_executor.js
@@ -1,19 +1,19 @@
 'use strict';
 
-const Order = require('../../dict/order')
-const _ = require('lodash')
-const moment = require('moment')
+const Order = require('../../dict/order');
+const _ = require('lodash');
+const moment = require('moment');
 
 module.exports = class OrderExecutor {
     constructor(exchangeManager, tickers, systemUtil, logger) {
-        this.exchangeManager = exchangeManager
-        this.tickers = tickers
-        this.logger = logger
-        this.systemUtil = systemUtil
-        this.runningOrders = {}
+        this.exchangeManager = exchangeManager;
+        this.tickers = tickers;
+        this.logger = logger;
+        this.systemUtil = systemUtil;
+        this.runningOrders = {};
 
-        this.tickerPriceInterval = 200
-        this.tickerPriceRetries = 40
+        this.tickerPriceInterval = 200;
+        this.tickerPriceRetries = 40;
 
         this.orders = []
     }
@@ -24,7 +24,7 @@ module.exports = class OrderExecutor {
     adjustOpenOrdersPrice() {
         let orders = this.orders.filter(order =>
             order.order.hasAdjustedPrice()
-        )
+        );
 
         // cleanup
         for (let orderId in this.runningOrders) {
@@ -34,10 +34,10 @@ module.exports = class OrderExecutor {
         }
 
         let visitExchangeOrder = async orderContainer => {
-            let exchange = this.exchangeManager.get(orderContainer.exchange)
+            let exchange = this.exchangeManager.get(orderContainer.exchange);
             if (!exchange) {
-                console.error('OrderAdjust: Invalid exchange:' + orderContainer.exchange)
-                delete this.runningOrders[orderContainer.id]
+                console.error('OrderAdjust: Invalid exchange:' + orderContainer.exchange);
+                delete this.runningOrders[orderContainer.id];
 
                 return
             }
@@ -45,53 +45,53 @@ module.exports = class OrderExecutor {
             // order not known by exchange cleanup
             let lastExchangeOrder = await exchange.findOrderById(orderContainer.id);
             if (!lastExchangeOrder || lastExchangeOrder.status !== 'open') {
-                this.logger.info('OrderAdjust: Unknown order cleanup: ' + JSON.stringify([orderContainer.exchangeOrder.id, orderContainer.exchange, orderContainer.symbol]))
+                this.logger.info('OrderAdjust: Unknown order cleanup: ' + JSON.stringify([orderContainer.exchangeOrder.id, orderContainer.exchange, orderContainer.symbol]));
 
                 // async issues? we are faster then exchange; even in high load: implement a global order management
                 // and filter out executed order (eg LIMIT order process)
                 this.orders = this.orders.filter(myOrder =>
                     myOrder.id !== orderContainer.id
-                )
+                );
 
-                delete this.runningOrders[orderContainer.id]
+                delete this.runningOrders[orderContainer.id];
 
                 return
             }
 
             if (orderContainer.id in this.runningOrders) {
-                this.logger.info('OrderAdjust: already running: ' + JSON.stringify([orderContainer.id, orderContainer.exchange, orderContainer.symbol]))
+                this.logger.info('OrderAdjust: already running: ' + JSON.stringify([orderContainer.id, orderContainer.exchange, orderContainer.symbol]));
                 return
             }
 
-            this.runningOrders[orderContainer.id] = new Date()
+            this.runningOrders[orderContainer.id] = new Date();
 
-            let price = await this.getCurrentPrice(orderContainer.exchange, orderContainer.order.symbol, orderContainer.order.side)
+            let price = await this.getCurrentPrice(orderContainer.exchange, orderContainer.order.symbol, orderContainer.order.side);
             if (!price) {
-                this.logger.info('OrderAdjust: No up to date ticker price found: ' + JSON.stringify([orderContainer.exchange, orderContainer.order.symbol, orderContainer.order.side]))
+                this.logger.info('OrderAdjust: No up to date ticker price found: ' + JSON.stringify([orderContainer.exchange, orderContainer.order.symbol, orderContainer.order.side]));
 
-                delete this.runningOrders[orderContainer.id]
+                delete this.runningOrders[orderContainer.id];
 
                 return
             }
 
-            let orderUpdate = Order.createPriceUpdateOrder(orderContainer.exchangeOrder.id, price)
+            let orderUpdate = Order.createPriceUpdateOrder(orderContainer.exchangeOrder.id, price);
 
             // normalize prices for positions compare; we can have negative prices depending on "side"
             if (Math.abs(lastExchangeOrder.price) === Math.abs(price)) {
-                this.logger.info('OrderAdjust: No price update needed:' + JSON.stringify([lastExchangeOrder.id, Math.abs(lastExchangeOrder.price), Math.abs(price), orderContainer.exchange, orderContainer.order.symbol]))
-                delete this.runningOrders[orderContainer.id]
+                this.logger.info('OrderAdjust: No price update needed:' + JSON.stringify([lastExchangeOrder.id, Math.abs(lastExchangeOrder.price), Math.abs(price), orderContainer.exchange, orderContainer.order.symbol]));
+                delete this.runningOrders[orderContainer.id];
 
                 return
             }
 
             try {
-                let updatedOrder = await exchange.updateOrder(orderUpdate.id, orderUpdate)
+                let updatedOrder = await exchange.updateOrder(orderUpdate.id, orderUpdate);
 
                 if (updatedOrder.status === 'open') {
                     this.logger.info('OrderAdjust: Order adjusted with orderbook price: ' + JSON.stringify([updatedOrder.id, Math.abs(lastExchangeOrder.price), Math.abs(price), orderContainer.exchange, orderContainer.order.symbol]))
-                } else if(updatedOrder.status === 'canceled' && updatedOrder.retry === true) {
+                } else if (updatedOrder.status === 'canceled' && updatedOrder.retry === true) {
                     // we update the price outside the orderbook price range on PostOnly we will cancel the order directly
-                    this.logger.error('OrderAdjust: Updated order canceled recreate: ' + JSON.stringify(orderContainer))
+                    this.logger.error('OrderAdjust: Updated order canceled recreate: ' + JSON.stringify(orderContainer));
 
                     // recreate order
                     // @TODO: resync used balance in case on order is partially filled
@@ -99,12 +99,12 @@ module.exports = class OrderExecutor {
                 } else {
                     this.logger.error('OrderAdjust: Unknown order state: ' + JSON.stringify(orderContainer))
                 }
-            } catch(err) {
+            } catch (err) {
                 this.logger.error('OrderAdjust: adjusted failed: ' + JSON.stringify([String(err), orderContainer, orderUpdate]))
             }
 
             delete this.runningOrders[orderContainer.id]
-        }
+        };
 
         return Promise.all(orders.map(order => {
             return visitExchangeOrder(order)
@@ -119,19 +119,19 @@ module.exports = class OrderExecutor {
 
     cancelOrder(exchangeName, orderId) {
         return new Promise(async resolve => {
-            let exchange = this.exchangeManager.get(exchangeName)
+            let exchange = this.exchangeManager.get(exchangeName);
             if (!exchange) {
-                console.error('Invalid exchange: ' + exchangeName)
+                console.error('Invalid exchange: ' + exchangeName);
 
-                resolve()
+                resolve();
                 return;
             }
 
             try {
                 resolve(await exchange.cancelOrder(orderId))
-            } catch(err) {
-                this.logger.error('Order cancel error: ' + orderId + ' ' + err)
-                console.log('Order error: ' + orderId + err)
+            } catch (err) {
+                this.logger.error('Order cancel error: ' + orderId + ' ' + err);
+                console.log('Order error: ' + orderId + err);
 
                 resolve()
             }
@@ -140,19 +140,19 @@ module.exports = class OrderExecutor {
 
     cancelAll(exchangeName, symbol) {
         return new Promise(async resolve => {
-            let exchange = this.exchangeManager.get(exchangeName)
+            let exchange = this.exchangeManager.get(exchangeName);
             if (!exchange) {
-                console.error('Invalid exchange: ' + exchangeName)
+                console.error('Invalid exchange: ' + exchangeName);
 
-                resolve()
+                resolve();
                 return;
             }
 
             try {
                 resolve(await exchange.cancelAll(symbol))
-            } catch(err) {
-                this.logger.error('Order cancel all error: ' + symbol)
-                console.log('Order all error: ' + symbol)
+            } catch (err) {
+                this.logger.error('Order cancel all error: ' + symbol);
+                console.log('Order all error: ' + symbol);
 
                 resolve()
             }
@@ -160,9 +160,9 @@ module.exports = class OrderExecutor {
     }
 
     async triggerOrder(resolve, exchangeName, order, retry = 0) {
-        if(retry > this.systemUtil.getConfig('order.retry', 4)) {
-            this.logger.error(`Retry (${retry}) creating order reached: ` + JSON.stringify(order))
-            resolve()
+        if (retry > this.systemUtil.getConfig('order.retry', 4)) {
+            this.logger.error(`Retry (${retry}) creating order reached: ` + JSON.stringify(order));
+            resolve();
             return
         }
 
@@ -170,46 +170,46 @@ module.exports = class OrderExecutor {
             this.logger.info(`Retry (${retry}) creating order: ` + JSON.stringify(order))
         }
 
-        let exchange = this.exchangeManager.get(exchangeName)
+        let exchange = this.exchangeManager.get(exchangeName);
         if (!exchange) {
-            console.error('Invalid exchange: ' + exchangeName)
+            console.error('Invalid exchange: ' + exchangeName);
 
-            resolve()
+            resolve();
             return
         }
 
         if (order.hasAdjustedPrice() === true) {
-            order = await this.createAdjustmentOrder(exchangeName, order)
+            order = await this.createAdjustmentOrder(exchangeName, order);
 
             if (!order) {
-                this.logger.error('Order price adjust failed:' + JSON.stringify([exchangeName, order]))
-                resolve()
+                this.logger.error('Order price adjust failed:' + JSON.stringify([exchangeName, order]));
+                resolve();
                 return
             }
         }
 
-        let exchangeOrder = undefined
+        let exchangeOrder = undefined;
         try {
             exchangeOrder = await exchange.order(order)
-        } catch(err) {
-            this.logger.error('Order create canceled:' + JSON.stringify(order) + ' - ' + JSON.stringify(String(err)))
+        } catch (err) {
+            this.logger.error('Order create canceled:' + JSON.stringify(order) + ' - ' + JSON.stringify(String(err)));
 
-            resolve()
+            resolve();
             return
         }
 
         if (exchangeOrder.status === 'canceled' && exchangeOrder.retry === false) {
-            this.logger.error('Order create canceled:' + JSON.stringify(order) + ' - ' + JSON.stringify(exchangeOrder))
+            this.logger.error('Order create canceled:' + JSON.stringify(order) + ' - ' + JSON.stringify(exchangeOrder));
 
-            resolve(exchangeOrder)
+            resolve(exchangeOrder);
             return
         }
 
         if (exchangeOrder.retry === true) {
-            this.logger.info('Order not placed force retry: ' + JSON.stringify(exchangeOrder))
+            this.logger.info('Order not placed force retry: ' + JSON.stringify(exchangeOrder));
 
             setTimeout(async () => {
-                let retryOrder = Order.createRetryOrder(order)
+                let retryOrder = Order.createRetryOrder(order);
 
                 await this.triggerOrder(resolve, exchangeName, retryOrder, ++retry)
             }, this.systemUtil.getConfig('order.retry_ms', 1500));
@@ -217,15 +217,15 @@ module.exports = class OrderExecutor {
             return
         }
 
-        this.logger.info('Order created: ' + JSON.stringify([exchangeOrder.id, exchangeName, exchangeOrder.symbol, order, exchangeOrder]))
-        console.log('Order created: ' + JSON.stringify([exchangeOrder.id, exchangeName, exchangeOrder.symbol]))
+        this.logger.info('Order created: ' + JSON.stringify([exchangeOrder.id, exchangeName, exchangeOrder.symbol, order, exchangeOrder]));
+        console.log('Order created: ' + JSON.stringify([exchangeOrder.id, exchangeName, exchangeOrder.symbol]));
 
         this.orders.push({
             'id': exchangeOrder.id,
             'exchange': exchangeName,
             'exchangeOrder': exchangeOrder,
             'order': order,
-        })
+        });
 
         resolve(exchangeOrder)
     }
@@ -237,13 +237,12 @@ module.exports = class OrderExecutor {
      * @param order
      * @returns {Promise<*>}
      */
-    createAdjustmentOrder(exchangeName, order)
-    {
+    createAdjustmentOrder(exchangeName, order) {
         return new Promise(async resolve => {
-            let price = await this.getCurrentPrice(exchangeName, order.symbol, order.side)
-            if(!price) {
-                this.logger.error('Stop creating order; can not find up to date ticker price: ' + JSON.stringify([exchangeName, order.symbol, order.side]))
-                resolve()
+            let price = await this.getCurrentPrice(exchangeName, order.symbol, order.side);
+            if (!price) {
+                this.logger.error('Stop creating order; can not find up to date ticker price: ' + JSON.stringify([exchangeName, order.symbol, order.side]));
+                resolve();
                 return
             }
 
@@ -259,8 +258,7 @@ module.exports = class OrderExecutor {
      * @param side
      * @returns {Promise<any>}
      */
-    getCurrentPrice(exchangeName, symbol, side)
-    {
+    getCurrentPrice(exchangeName, symbol, side) {
         return new Promise(async resolve => {
             let wait = (time) => {
                 return new Promise(resolve => {
@@ -268,12 +266,12 @@ module.exports = class OrderExecutor {
                         resolve();
                     }, time);
                 });
-            }
+            };
 
-            let ticker = undefined
+            let ticker = undefined;
 
             for (let retry = 0; retry < this.tickerPriceRetries; retry++) {
-                ticker = this.tickers.getIfUpToDate(exchangeName, symbol, 10000)
+                ticker = this.tickers.getIfUpToDate(exchangeName, symbol, 10000);
                 if (ticker) {
                     break
                 }
@@ -287,17 +285,17 @@ module.exports = class OrderExecutor {
             }
 
             if (!ticker) {
-                this.logger.error('OrderExecutor: ticker price not found: ' + JSON.stringify([exchangeName, symbol, side]))
-                resolve()
+                this.logger.error('OrderExecutor: ticker price not found: ' + JSON.stringify([exchangeName, symbol, side]));
+                resolve();
                 return
             }
 
-            let price = ticker.bid
-            if(side === 'short') {
+            let price = ticker.bid;
+            if (side === 'short') {
                 price = ticker.ask * -1
             }
 
             resolve(price)
         })
     }
-}
+};
