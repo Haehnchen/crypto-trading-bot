6f3a1e5c9822b502d3f30e10da7eedf3e1faef84
Phuc Nguyen Minh <minhphuc429@gmail.com>
2019-12-15 00:13:46 +0700

    fix 'db' is not defined in class TickerLogRepository

--- modules/repository/ticker_log_repository.js
+++ modules/repository/ticker_log_repository.js
@@ -7,9 +7,9 @@ module.exports = class TickerLogRepository {
         this.db = db
     }
 
-    cleanOldLogEntries(days = 14) {
-        return new Promise((resolve) => {
-            const stmt = db.prepare('DELETE FROM ticker_log WHERE income_at < $income_at');
+  cleanOldLogEntries(days = 14) {
+    return new Promise(resolve => {
+      const stmt = this.db.prepare('DELETE FROM ticker_log WHERE income_at < $income_at');
 
             stmt.run({
                 'income_at': moment().subtract(days, 'days').unix()
