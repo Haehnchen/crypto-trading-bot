b6ccc112e74d0171b87b4dd293f7415b86a598a5
Daniel Espendiller <daniel@espendiller.net>
2019-12-20 20:21:09 +0100

    move binance init script to vcs

--- utils/instance_util.js
+++ utils/instance_util.js
@@ -10,11 +10,13 @@ module.exports = {
     return new Promise(resolve => {
       request('https://ftx.com/api/markets', (_error, _res, body) => {
         if (_error) {
+          resolve([]);
           return;
         }
 
         const response = JSON.parse(body);
         if (!response.result) {
+          resolve([]);
           return;
         }
 
@@ -29,6 +31,52 @@ module.exports = {
               symbol: pair.name,
               periods: ['1m', '15m', '1h'],
               exchange: 'ftx',
+              state: 'watch'
+            };
+
+            if (callback) {
+              result = callback(result, pair);
+            }
+
+            if (result) {
+              pairs.push(result);
+            }
+          });
+
+        resolve(pairs);
+      });
+    });
+  },
+
+  /**
+   * Init helper for FTX exchange to fetch usd based "perp" contract pairs with a callback to ignore and add trading options
+   * @param callback
+   * @returns {Promise<unknown>}
+   */
+  binanceInitUsd: async function(callback) {
+    return new Promise(resolve => {
+      request('https://api.binance.com/api/v1/exchangeInfo', (_error, _res, body) => {
+        const pairs = [];
+
+        const content = JSON.parse(body);
+
+        if (!content.symbols) {
+          resolve([]);
+          return;
+        }
+
+        content.symbols
+          .filter(
+            p =>
+              p.quoteAsset === 'USDT' &&
+              !['USDC', 'PAX', 'USDS', 'TUSD'].includes(p.baseAsset) &&
+              p.status.toLowerCase() === 'trading'
+          )
+          .forEach(pair => {
+            let result = {
+              symbol: pair.symbol,
+              periods: ['1m', '15m', '1h'],
+              exchange: 'binance',
               state: 'watch'
             };
 
