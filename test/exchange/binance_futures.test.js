c58a13a5791581977b5b0884ee8658d961d2a292
Daniel Espendiller <daniel@espendiller.net>
2020-07-09 19:32:42 +0200

    support Binance futures position close via websocket

--- test/exchange/binance_futures.test.js
+++ test/exchange/binance_futures.test.js
@@ -1,14 +1,15 @@
 const assert = require('assert');
 const fs = require('fs');
 const BinanceFutures = require('../../src/exchange/binance_futures');
+const Position = require('../../src/dict/position');
 
 describe('#binance_futures exchange implementation', () => {
-  const getPositions = () => {
-    return JSON.parse(fs.readFileSync(`${__dirname}/binance_futures/positions.json`, 'utf8'));
+  const getJsonFixture = filename => {
+    return JSON.parse(fs.readFileSync(`${__dirname}/binance_futures/${filename}`, 'utf8'));
   };
 
   it('calculates positions', () => {
-    const positions = BinanceFutures.createPositions(getPositions());
+    const positions = BinanceFutures.createPositions(getJsonFixture('positions.json'));
 
     assert.strictEqual(positions[0].getSymbol(), 'ETHUSDT');
     assert.strictEqual(positions[0].isShort(), true);
@@ -23,5 +24,19 @@ describe('#binance_futures exchange implementation', () => {
     // long
     assert.strictEqual(positions[2].getProfit(), 1.52);
     assert.strictEqual(positions[3].getProfit(), -9.15);
+  });
+
+  it('websocket position close positions', async () => {
+    const binanceFutures = new BinanceFutures({}, {}, {}, { info: () => {} }, {}, {});
+
+    binanceFutures.positions = {
+      EOSUSDT: new Position('EOSUSDT', 'long', 1),
+      ADAUSDT: new Position('ADAUSDT', 'long', 1)
+    };
+
+    const json = getJsonFixture('websocket_position_close.json');
+    binanceFutures.accountUpdate(json);
+
+    assert.strictEqual((await binanceFutures.getPositions()).length, 1);
   });
 });
